00de0bd145a2c390d61e71a16764c7f472fc40a2
Pushpender Singh <pushpender272320@gmail.com>
2025-07-06 16:40:19 +0530

    Completed Transaction Page

--- src/vr-exp-webapp-frontend/src/pages/Transaction.jsx
+++ src/vr-exp-webapp-frontend/src/pages/Transaction.jsx
@@ -1,58 +1,11 @@
-import CancelBtn from "../widgets/CancelBtn";
 import Footer from "../widgets/Footer";
-
-// const dummyData = [["Roller Coaster World", "4 ICP", "22-04-2025", "Buy"],
-// ["Roller Coaster World", "4 ICP", "22-04-2025", "Sell"],
-// ["Roller Coaster World", "4 ICP", "22-04-2025", "Buy"],
-// ["Roller Coaster World", "4 ICP", "22-04-2025", "Sell"]];
-
-const dummyData = [];
+import TxnTable from "../widgets/TxnTable";
 
 export default function TransactionPage() {
-	let content = null;
-
-	if (dummyData.length == 0) {
-		content = <section id="txn-list" className="w-7xl mt-14 mb-26 bg-[#ffffff03] border-2 border-[#BED1D920] rounded-2xl">
-			{/* Heading */}
-			<div className="w-7xl px-4 bg-[#43A7CB20] border-b-2 border-[#BED1D920] rounded-t-2xl flex items-center text-[#BED1D9] text-[20px] font-bold">
-				<div className="w-[110px] py-3 flex justify-center">S No.</div>
-				<div className="w-[470px] py-3 flex justify-center">Item Name</div>
-				<div className="w-[160px] py-3 flex justify-center">Price</div>
-				<div className="w-[260px] py-3 flex justify-center">Date</div>
-				<div className="w-[130px] py-3 flex justify-center">Type</div>
-				<div className="w-[130px] py-3 flex justify-center">Delete</div>
-			</div>
-
-			<div className="h-96 flex justify-center items-center text-[#BED1D950] text-2xl tracking-wider">Nothing to show</div>
-		</section>
-	} else {
-		content = <section id="txn-list" className="w-7xl mt-14 mb-26 bg-[#ffffff03] border-2 border-[#BED1D920] rounded-2xl">
-			{/* Heading */}
-			<div className="w-7xl px-4 bg-[#43A7CB20] border-b-2 border-[#BED1D920] rounded-t-2xl flex items-center text-[#BED1D9] text-[20px] font-bold">
-				<div className="w-[110px] py-3 flex justify-center">S No.</div>
-				<div className="w-[470px] py-3 flex justify-center">Item Name</div>
-				<div className="w-[160px] py-3 flex justify-center">Price</div>
-				<div className="w-[260px] py-3 flex justify-center">Date</div>
-				<div className="w-[130px] py-3 flex justify-center">Type</div>
-				<div className="w-[130px] py-3 flex justify-center">Delete</div>
-			</div>
-			{/* Entries */}
-			{dummyData.map((data, index) => (
-				<div className="w-7xl px-4 bg-white/3 border-b-2 border-[#BED1D920] flex items-center text-[#BED1D9] text-[18px]">
-					<div className="w-[110px] py-3 flex justify-center">{(index + 1).toString() + "."}</div>
-					<div className="w-[470px] py-3 flex justify-center">{data[0]}</div>
-					<div className="w-[160px] py-3 flex justify-center">{data[1]}</div>
-					<div className="w-[260px] py-3 flex justify-center">{data[2]}</div>
-					<div className="w-[130px] py-3 flex justify-center">{data[3]}</div>
-					<div className="w-[130px] py-3 flex justify-center"><CancelBtn btnHandler={() => { }} /></div>
-				</div>
-			))}
-		</section>
-	}
 
 	return (
 		<div className="flex flex-col items-center">
-			{content}
+			<TxnTable />
 
 			<Footer />
 		</div>
---
+++ src/vr-exp-webapp-frontend/src/widgets/TxnTable.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { vr_exp_webapp_backend } from "../../../declarations/vr-exp-webapp-backend";
+import CancelBtn from "./CancelBtn";
+
+export default function TxnTable() {
+	async function getPrincipalID() {	// Returns Principal ID from cache
+		const principal_id = localStorage.getItem("principal_id");
+		if (principal_id == null) {
+			naviagtor("/");
+		}
+		console.log(principal_id);
+		return principal_id;
+	}
+
+	async function getRecords() {	// Fetches Transaction data from the backend canister
+		const principal_id = await getPrincipalID();
+		const txn = await vr_exp_webapp_backend.get_transactions(principal_id);
+		setTxnRecord(txn);
+		await getData(txn);
+	}
+
+	async function getData(txn_record) {	// Fetches NFT data from the backend canister
+		let _nftData = [];
+		for (let i=0; i<txn_record.length; i++) {
+			const nft = await vr_exp_webapp_backend.get_nft_data(txn_record[i].nft_token_id);
+			_nftData.push(nft[0]);
+		}
+		setNftData(_nftData);
+		console.log(_nftData[0]);
+	}
+
+	const [txnRecord, setTxnRecord] = useState([]);
+	const [nftData, setNftData] = useState([]);
+	const [tries, setTries] = useState(0);
+	const naviagtor = useNavigate();
+
+	// Fetching Data
+	if ((txnRecord.length == 0) && (tries < 5)) {
+		getRecords();
+		console.log(tries);
+		setTries(tries + 1);
+	}
+
+	let content = null;
+	if ((txnRecord.length == 0) || (nftData.length == 0)) {
+		content = <div className="h-96 flex justify-center items-center text-[#BED1D950] text-2xl tracking-wider">Nothing to show</div>;
+	} else {
+		console.log(nftData);
+		content = nftData.map((data, index) => (
+			<div className="w-7xl px-4 bg-white/3 border-b-2 border-[#BED1D920] flex items-center text-[#BED1D9] text-[18px]">
+				<div className="w-[110px] py-3 flex justify-center">{(index + 1).toString() + "."}</div>
+				<div className="w-[470px] py-3 flex justify-center">{data.item_name}</div>
+				<div className="w-[160px] py-3 flex justify-center">{data.price.toString() + " ICP"}</div>
+				<div className="w-[260px] py-3 flex justify-center">{data.time_stamp.split(" ")[0]}</div>
+				<div className="w-[130px] py-3 flex justify-center">{txnRecord[index].txn_type}</div>
+				<div className="w-[130px] py-3 flex justify-center"><CancelBtn btnHandler={() => { }} /></div>
+			</div>
+		))
+	}
+
+
+	return (
+		<section id="txn-list" className="w-7xl mt-14 mb-36 bg-[#ffffff03] border-2 border-[#BED1D920] rounded-2xl">
+			{/* Heading */}
+			<div className="w-7xl px-4 bg-[#43A7CB20] border-b-2 border-[#BED1D920] rounded-t-2xl flex items-center text-[#BED1D9] text-[20px] font-bold">
+				<div className="w-[110px] py-3 flex justify-center">S No.</div>
+				<div className="w-[470px] py-3 flex justify-center">Item Name</div>
+				<div className="w-[160px] py-3 flex justify-center">Price</div>
+				<div className="w-[260px] py-3 flex justify-center">Date</div>
+				<div className="w-[130px] py-3 flex justify-center">Type</div>
+				<div className="w-[130px] py-3 flex justify-center">Delete</div>
+			</div>
+
+			{content}
+		</section>
+	);
+}
