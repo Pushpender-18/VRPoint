e1054ca2c243585ca192ef2d908c5d279af62505
Pushpender Singh <pushpender272320@gmail.com>
2025-07-10 12:58:25 +0530

    Removed delete button from txn

--- src/vr-exp-webapp-frontend/src/widgets/TxnTable.jsx
+++ src/vr-exp-webapp-frontend/src/widgets/TxnTable.jsx
@@ -53,11 +53,10 @@ export default function TxnTable() {
 			return (
 				<div className={classString}>
 					<div className="w-[110px] py-3 flex justify-center">{(index + 1).toString() + "."}</div>
-					<div className="w-[470px] py-3 flex justify-center">{data.item_name}</div>
+					<div className="w-[450px] py-3 flex justify-center overflow-ellipsis">{data.item_name}</div>
 					<div className="w-[160px] py-3 flex justify-center">{data.price.toString() + " ICP"}</div>
-					<div className="w-[260px] py-3 flex justify-center">{data.time_stamp.split(" ")[0]}</div>
+					<div className="w-[310px] py-3 flex justify-center">{data.time_stamp.split(" ")[0]}</div>
 					<div className="w-[130px] py-3 flex justify-center">{txnRecord[index].txn_type}</div>
-					<div className="w-[130px] py-3 flex justify-center"><CancelBtn btnHandler={() => { }} /></div>
 				</div>
 			)
 		})
@@ -68,11 +67,10 @@ export default function TxnTable() {
 			{/* Heading */}
 			<div className="w-7xl px-4 bg-[#43A7CB20] border-b-2 border-[#BED1D920] rounded-t-2xl flex items-center text-[#BED1D9] text-[20px] font-bold">
 				<div className="w-[110px] py-3 flex justify-center">S No.</div>
-				<div className="w-[470px] py-3 flex justify-center">Item Name</div>
+				<div className="w-[450px] py-3 flex justify-center">Item Name</div>
 				<div className="w-[160px] py-3 flex justify-center">Price</div>
-				<div className="w-[260px] py-3 flex justify-center">Date</div>
+				<div className="w-[310px] py-3 flex justify-center">Date</div>
 				<div className="w-[130px] py-3 flex justify-center">Type</div>
-				<div className="w-[130px] py-3 flex justify-center">Delete</div>
 			</div>
 
 			{content}
